65dc819101153a5b82ee470d7a6757a7dd6bd3c8
Kaila <kailaamia@gmail.com>
2023-11-28 18:13:38 -0600

    Added edge cases for logotext length

--- index.js
+++ index.js
@@ -5,8 +5,16 @@ const getLogo = require('./lib/questions')
 function init(){
     inquirer.prompt(getLogo)
     .then(answers => {
-        const content = generateSvg.generateSvg(answers)
-        writeToFile(`./examples/${answers.logoText}.svg`, content)
+        if (answers.logoText.length > 3){
+           console.log('Logo text must be less than 3 characters. Try again')
+            init()
+        } else if (answers.logoText === 0){
+            console.log('Logo text must be up to 3 characters. Try again')
+            init()
+        } else{
+            const content = generateSvg.generateSvg(answers)
+            writeToFile(`./examples/${answers.logoText}.svg`, content)
+        }
     })
 }
 
